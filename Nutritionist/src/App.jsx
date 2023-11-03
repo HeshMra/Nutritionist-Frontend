7d591c62783b5291215b0903c7d597b3611959dc
heshith <heshith619@gmail.com>
2023-11-03 17:19:41 +0530

    navigation bar

--- Nutritionist/src/App.jsx
+++ Nutritionist/src/App.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Team from './Pages/Team'
-import Process from './Pages/Process'
-import Pricing from './Pages/Pricing'
-import Blog from './Pages/Blog'
-import Contactus from './Pages/Contactus'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Team from "./Pages/Team";
+import Process from "./Pages/Process";
+import Pricing from "./Pages/Pricing";
+import Blog from "./Pages/Blog";
+import Contactus from "./Pages/Contactus";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    // <div className='text-3xl font-bold underline'>App</div> //for check the css
-    <Routes>
-      <Route path='' element={<Home/>}/>
-      <Route path='/About' element={<About/>}/>
-      <Route path='/Team' element={<Team/>}/>
-      <Route path='/Process' element={<Process/>}/>
-      <Route path='/Pricing' element={<Pricing/>}/>
-      <Route path='/Blog' element={<Blog/>}/>
-      <Route path='/Contactus' element={<Contactus/>}/>
-    </Routes>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Team" element={<Team />} />
+        <Route path="/Process" element={<Process />} />
+        <Route path="/Pricing" element={<Pricing />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Contactus" element={<Contactus />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ Nutritionist/src/Components/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="h-[95px] bg-[#1A3129] flex justify-between items-center px-6">
+      <div className="flex items-center p-16 mx-8">
+        <img
+          src="Images/Logo.png"
+          alt="Logo"
+          className="w-[194.62px] h-[45px] cursor-pointer "
+        />
+      </div>
+      <ul className="flex space-x-5  p-16 mx-8  items-center">
+        <li className="list-none">
+          <a href="/" className="no-underline text-white px-2 hover:text-[#CBEA7B]">
+            Home
+          </a>
+        </li>
+        <li className="list-none">
+          <a href="/" className="no-underline text-white px-2 hover:text-[#CBEA7B]">
+            About
+          </a>
+        </li>
+        <li className="list-none">
+          <a href="/" className="no-underline text-white px-2 hover:text-[#CBEA7B]">
+            Team
+          </a>
+        </li>
+        <li className="list-none">
+          <a href="/" className="no-underline text-white px-2 hover:text-[#CBEA7B]">
+            Process
+          </a>
+        </li>
+        <li className="list-none">
+          <a href="/" className="no-underline text-white px-2 hover:text-[#CBEA7B]">
+            Pricing
+          </a>
+        </li>
+        <li className="list-none">
+          <a href="/" className="no-underline text-white px-2 hover:text-[#CBEA7B]">
+            Blog
+          </a>
+        </li>
+        <li className="list-none">
+          <a href="/" className="no-underline text-white px-2 hover:text-[#CBEA7B]">
+            Contact Us
+          </a>
+        </li>
+        <div className="px-2">
+          <a
+            href="##"
+            class="bg-[#CBEA7B] rounded-lg py-3.5 font-medium inline-block mr-4 
+           hover:border-yellow-500 hover:text-white hover:border w-[137px] h-[55px] leading-[27px] text-center "
+          >
+            Contact Us
+          </a>
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
