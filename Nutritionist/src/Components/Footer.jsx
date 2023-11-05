3d8c9caed7530026ee9c32d044fd29f3225643bf
heshith <heshith619@gmail.com>
2023-11-05 14:38:51 +0530

    Hero section

--- Nutritionist/src/Components/Footer.jsx
+++ Nutritionist/src/Components/Footer.jsx
@@ -7,8 +7,8 @@ import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <div className="bg-[#1A3129] h-[299px]">
-      <nav class="flex items-center p-16 mx-8">
+    <div className="bg-[#1A3129] h-[299px] ">
+      <footer class="flex items-center p-16 mx-8 ">
         <img
           src="images/logo.png"
           class="w-[194.62px] h-[45px] cursor-pointer "
@@ -51,12 +51,13 @@ const Footer = () => {
           </li>
         </ul>
         <a href="##" class=" text-white px-6">
-          Got To Top
+          Got To Top 
         </a>
-      </nav>
+        <BsFillArrowUpCircleFill style={{ color: "#CBEA7B" }} className="w-12 h-12 relative ml-4  mr-4"/>
+      </footer>
 
       <div className="bg-[#2C5446] h-[87px] rounded-lg shadow border border-[#356554] mx-28 flex items-center pl-5 ">
-        <div className="flex-1 flex space-x-5">
+        <div className="flex-1 flex space-x-5 ">
           <div className="bg-[#2C5446] w-[223px] h-[55px] text-white flex items-center justify-center rounded-lg shadow border border-[#356554]">
             <FaEnvelope
               style={{ color: "#CBEA7B" }}
@@ -81,7 +82,7 @@ const Footer = () => {
             <p className="flex items-center ml-1.5">Somewhere in the World</p>
           </div>
         </div>
-        <p className="text-white p-5 flex items-center">© 2023 Nutritionist. All rights reserved.  <BsFillArrowUpCircleFill style={{ color: "#CBEA7B" }} className="w-8 h-8 relative ml-5"/></p>
+        <p className="text-white p-5 flex items-center">© 2023 Nutritionist. All rights reserved.  </p>
       </div>
     </div>
   );
--- Nutritionist/src/Pages/Home.jsx
+++ Nutritionist/src/Pages/Home.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div className="flex ">
+        <img
+          src="Images/cover.png"
+          alt="cover"
+          className="w-1/2 h-1/2 rounded-br-[50px] mt-16"
+        ></img>
+        <div className="ml-12 mt-40">
+          <p className="text-[28px]  font-semibold ">
+            Transform Your ❤️ Health with
+          </p>
+          <h1 className="mt-3.5  text-[58px] font-semibold leading-tight">
+            Personalized Nutrition Coaching
+          </h1>
+          <p className="mt-5  ">
+            Welcome to Nutritionist, your partner in achieving optimal health
+            through personalized nutrition
+            <br /> coaching. Our certified nutritionists are here to guide you
+            on your weight loss journey, providing <br />
+            customized plans and ongoing support. Start your transformation
+            today and experience the <br /> power of personalized nutrition
+            coaching.
+          </p>
+          <div className="flex mt-12">
+            <div className="px-2">
+              <a
+                href="##"
+                class="  bg-[#CBEA7B] rounded-lg py-3.5 font-medium inline-block mr-4 
+           hover:border-yellow-500 hover:text-white hover:border w-[187px] h-[59px] leading-[27px] text-center "
+              >
+                Get Starter Today
+              </a>
+            </div>
 
-export default Home
+            <div>
+              <a
+                href="##"
+                class="bg-[#F6FBE9]shadow border border-[#E5F5BD] rounded-lg py-3.5 font-medium inline-block mr-4 
+             w-[154px] h-[59px] leading-[27px] text-center "
+              >
+                Book a Demo
+              </a>
+            </div>
+          </div>
+
+          <div className="flex items-center mt-16">
+            <img src="Images/Container.png" alt="cover" className=""></img>
+            <p className="ml-2 font-medium">
+              <span className="text-[#468671]">430+</span> Happy Customers
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
