535519f3b5a289e49dde085bb463f9b0b893f287
heshith <heshith619@gmail.com>
2023-11-06 12:27:16 +0530

    Features Section

--- Nutritionist/src/Pages/Home.jsx
+++ Nutritionist/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
+      {/* Hero section */}
       <div className="flex ">
         <img
           src="Images/cover.png"
@@ -51,6 +52,119 @@ const Home = () => {
             <img src="Images/Container.png" alt="cover" className=""></img>
             <p className="ml-2 font-medium">
               <span className="text-[#468671]">430+</span> Happy Customers
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* second section */}
+      <div className="text-center mt-36">
+        <h1 className="text-5xl font-semibold ">Features</h1>
+        <p className="mt-2.5">
+          Welcome to the Feature Section of Nutritionist, your ultimate
+          destination for all things nutrition and wellness.
+        </p>
+      </div>
+
+      <div className="mx-40 mt-20 flex ">
+        <div>
+          <div className="w-[680px] h-[250px] bg-[#F6FBE9] shadow border border-[#E5F5BD] rounded-xl ">
+            <div className="flex items-center mx-10 my-12">
+              <img src="Images/Icon Container.png" alt="" />
+              <h1 className="text-2xl font-semibold ml-3.5">
+                Personalized Nutrition Plans
+              </h1>
+            </div>
+
+            <p className="mx-10  mt-[-25px] ">
+              Receive a tailored nutrition plan designed specifically for your
+              body and goals. Our <br /> certified nutritionists will consider
+              your unique needs, dietary preferences, and health 
+              conditions to create a plan that suits you best.
+            </p>
+          </div>
+
+          <div className="w-[680px] h-[250px] bg-[#F6FBE9] shadow border border-[#E5F5BD] rounded-xl mt-6">
+            <div className="flex items-center mx-10 my-12">
+              <img src="Images/Icon Container-1.png" alt="" />
+              <h1 className="text-2xl font-semibold ml-3.5">
+              Food Tracking and Analysis
+              </h1>
+            </div>
+
+            <p className="mx-10  mt-[-25px] ">
+            Effortlessly track your food intake using our user-friendly app. 
+            Our nutritionists will analyze your data to provide insights into your 
+            eating habits, help you identify areas for improvement, and make personalized 
+            recommendations.
+            </p>
+          </div>
+
+          <div className="w-[680px] h-[250px] bg-[#F6FBE9] shadow border border-[#E5F5BD] rounded-xl mt-6">
+            <div className="flex items-center mx-10 my-12">
+              <img src="Images/Icon Container-3.png" alt="" />
+              <h1 className="text-2xl font-semibold ml-3.5">
+              Lifestyle and Behavior Coaching
+              </h1>
+            </div>
+
+            <p className="mx-10  mt-[-25px] ">
+             Achieving sustainable results requires more than just a diet plan.
+             Our nutritionists will work with you to develop healthy habits, 
+             address emotional eating, and provide strategies to overcome obstacles 
+             along the way.
+            </p>
+          </div>
+
+        </div>
+
+        
+
+        {/* second column */}
+        <div className="ml-6">
+        <div>
+          <div className="w-[680px] h-[250px] bg-[#F6FBE9] shadow border border-[#E5F5BD] rounded-xl ">
+            <div className="flex items-center mx-10 my-12">
+              <img src="Images/Icon Container-0.png" alt="" />
+              <h1 className="text-2xl font-semibold ml-3.5">
+              Guidance from Certified Nutritionists              </h1>
+            </div>
+
+            <p className="mx-10  mt-[-25px] ">
+            Our team of experienced and certified nutritionists will provide professional guidance 
+            and support throughout your journey. They will answer your questions, address your concerns, 
+            and keep you motivated as you work towards your goals.
+            </p>
+          </div>
+        </div>
+
+
+
+         <div className="w-[680px] h-[250px] bg-[#F6FBE9] shadow border border-[#E5F5BD] rounded-xl mt-6">
+            <div className="flex items-center mx-10 my-12">
+              <img src="Images/Icon Container-2.png" alt="" />
+              <h1 className="text-2xl font-semibold ml-3.5">
+              Meal Planning and Recipes              </h1>
+            </div>
+
+            <p className="mx-10  mt-[-25px] ">
+            Access a vast collection of delicious and healthy recipes tailored to your dietary needs.
+             Our nutritionists will also create personalized meal plans, making it easier for you to 
+             stay on track and enjoy nutritious meals.
+            </p>
+          </div>
+          <div className="w-[680px] h-[250px] bg-[#F6FBE9] shadow border border-[#E5F5BD] rounded-xl mt-6">
+            <div className="flex items-center mx-10 my-12">
+              <img src="Images/Icon Container-4.png" alt="" />
+              <h1 className="text-2xl font-semibold ml-3.5">
+              Nutritional Education and Workshops
+              </h1>
+            </div>
+
+            <p className="mx-10  mt-[-25px] ">
+            Expand your knowledge of nutrition through informative articles and educational workshops. 
+            Our nutritionists will equip you with the knowledge and tools to make informed choices for 
+            long-term success.
             </p>
           </div>
         </div>
